60e77a907a1de54b6d26afedde5b4118899d5237
shanil <shanil@perfectomobile.com>
2018-09-23 23:41:00 +0300

    Initial Commit

---
+++ LiftingLog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiftingLog
+//
+//  Created by Shani Levinkind on 23/09/2018.
+//  Copyright © 2018 Shani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
